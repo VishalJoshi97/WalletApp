dbb7a8c18edc73cace36f44ab5c7b1396db2cf33
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2025-07-19 15:55:24 +0530

    cron.js added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,14 @@ import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from "./routes/transactionsRoute.js"
 import initDB from "./config/db.js"
 
+import job from "./config/cron.js"
+
 dotenv.config()
 
+ 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
+if(process.env.NODE_ENV==="production") job.start()
 
 //middleware-it is a fun that runs in the middle b/w the req and res
 //Ex: if u send a req to ig,then res will be given if ur authorized user
@@ -24,7 +27,11 @@ app.use(rateLimiter)
 // })
 
 // console.log("my Port",process.env.PORT);
+const PORT = process.env.PORT || 5001;
 
+application.get("/api/health", (req, res) => {
+    res.status(200).json({status:"ok"})
+})
 
 // for HEALTH-CHECK-basics
 // app.get("/", (req, res) => {
